716e55dee8cef674053d25094a675a795bcbc1c4
나이스탑 <mistarcodm@gmail.com>
2025-05-21 17:04:38 +0900

    Add files via upload

--- canary.c
+++ canary.c
@@ -1,24 +1,30 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-void win(){
-    system("/bin/sh");
+void init(){
+	setvbuf(stdin, NULL, _IONBF, 0);
+	setvbuf(stdout, NULL, _IONBF, 0);
+	setvbuf(stderr, NULL, _IONBF, 0);
 }
 
-void init() {
-    setvbuf(stdout, NULL, _IONBF, 0);
-    setvbuf(stdin, NULL, _IONBF, 0);
-    setvbuf(stderr, NULL, _IONBF, 0);
+void win(){
+	system("/bin/sh");
 }
 
 int main(){
-    init();
-    char buf[0x30];
-    printf("input buf: ");
-    scanf("%s", buf);
-    printf("input buf: ");
-    scanf("%s", buf);
-    return 0;
+
+	init();	
+	char buf[100] = {0,};
+
+	read(0, buf, 1000);
+
+	printf("Your Input : %s\n", buf);
+	printf("Second Input : ");
+
+	gets(buf);
+
+	printf("Your Second Input : %s\n", buf);
+
+	return 0;
 }
